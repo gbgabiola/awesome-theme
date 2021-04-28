912264427e59613c6e9904a3c40e28e41769145c
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2021-04-28 16:40:42 +0800

    Add and create post formats

--- index.php
+++ index.php
@@ -4,14 +4,8 @@ get_header();
 
   if(have_posts()) {
     while(have_posts()) {
-      the_post(); ?>
-      <h3><?php the_title(); ?></h3>
-      <div class="thumbnail-img"><?php the_post_thumbnail('large'); ?></div>
-      <small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
-      <p><?php the_content(); ?></p>
-      <hr>
-
-    <?php
+      the_post();
+      get_template_part('content', get_post_format());
     }
   }
 
