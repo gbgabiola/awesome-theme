3a317c4058f219a7f55a910891a9081c09a5e36e
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2021-04-28 17:59:56 +0800

    Integrate Bootstrap

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
-  <footer>
-    <p>This us my footer</p>
-    <?php wp_nav_menu(array('theme_location' => 'secondary')); ?>
-  </footer>
+    <footer>
+      <p>This us my footer</p>
+      <?php wp_nav_menu(array('theme_location' => 'secondary')); ?>
+    </footer>
+  </div><!-- .container -->
   <?php wp_footer(); ?>
+
 </body>
 </html>
