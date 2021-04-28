cce15a1eed155840fba97cdbcc824194c44423fa
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2021-04-29 00:26:39 +0800

    Implement Bootstrap carousel and grid

--- page-home.php
+++ page-home.php
@@ -1,36 +1,72 @@
 <?php get_header(); ?>
 
   <div class="row">
-    <?php
-    $args_cat = array(
-      'include' => '6, 7, 5'
-    );
-    $categories = get_categories($args_cat);
+    <div class="col-xs-12">
+		  <div id="awesome-carousel" class="carousel slide" data-ride="carousel">
 
-    foreach ($categories as $category) {
-      $args = array(
-        'type' => 'post',
-        'posts_per_page' => 1,
-        'category__in' => $category->term_id,
-        'category__not_in' => array(8),
-      );
+        <!-- Slides wrapper -->
+        <div class="carousel-inner" role="listbox">
+          <?php
+          $args_cat = array(
+            'include' => '6, 7, 5'
+          );
+          $categories = get_categories($args_cat);
+          $count = 0;
+				  $bullets = '';
 
-      $lastBlog = new WP_Query($args);
+          foreach ($categories as $category) {
+            $args = array(
+              'type' => 'post',
+              'posts_per_page' => 1,
+              'category__in' => $category->term_id,
+              'category__not_in' => array(8),
+            );
 
-      if ($lastBlog->have_posts()) {
-        while ($lastBlog->have_posts()) {
-          $lastBlog->the_post(); ?>
+            $lastBlog = new WP_Query($args);
 
-          <div class="col-xs-12 col-sm-4">
-            <?php get_template_part('content', 'featured'); ?>
-          </div>
-        <?php
-        }
-      }
+            if ($lastBlog->have_posts()) {
+              while ($lastBlog->have_posts()) {
+                $lastBlog->the_post(); ?>
 
-      wp_reset_postdata();
-    }
-    ?>
+                <div class="item <?php if ($count == 0) : echo 'active'; endif; ?>">
+						      <?php the_post_thumbnail('full'); ?>
+						      <div class="carousel-caption">
+							      <?php the_title(sprintf('<h1 class="entry-title"><a href="%s">', esc_url(get_permalink())), '</a></h1>'); ?>
+								    <small><?php the_category(' '); ?></small>
+						      </div>
+						    </div>
+
+                <?php
+                // $bullets .= '<li data-target="#awesome-carousel" data-slide-to="' . $count . '" class="';
+                // if ($count == 0) : $bullets .= 'active'; endif;
+                // $bullets .= '"></li>';
+
+                // Using ternary operator
+                $bullets .= '<li data-target="#awesome-carousel" data-slide-to="' . $count . '" class="' . (($count == 0) ? 'active' : '') . '"></li>';
+              }
+            }
+
+            wp_reset_postdata();
+            $count++;
+          }
+          ?>
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <?php echo $bullets; ?>
+          </ol>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#awesome-carousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#awesome-carousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
   </div>
 
   <div class="row">
